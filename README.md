488345c3bc32d397888522cef38ac93f1376603e
Joaquin Bejar Garcia <joaquin.bejar@gmail.com>
2018-05-22 06:04:45 +0200

    23 commit

--- README.md
+++ README.md
@@ -469,3 +469,4 @@ You can feed Sparklint an event log file to playback activities.
  
  
  
+ 
--- src/main/scala/com/groupon/sparklint/ui/UIEventSourceNavigation.scala
+++ src/main/scala/com/groupon/sparklint/ui/UIEventSourceNavigation.scala
@@ -96,4 +96,4 @@ object UIEventSourceNavigation {
 
   private def uniqueId(appId: String, idType: String) = s"$appId-$idType"
 }
-                                                                                                                                                                        
+                                                                                                                                                                          
